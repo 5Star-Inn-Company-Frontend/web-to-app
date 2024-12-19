4e4a98465ff820b5785f67ae82e9011f450a946c
Godwin Opara <godwinopara62@gmail.com>
2024-12-19 17:50:26 +0100

    styled reset password

--- src/components/auth/partnership.tsx
+++ src/components/auth/partnership.tsx
@@ -2,9 +2,9 @@
 const Partnership = () => {
     return (
         <div className="mt-5 flex justify-center items-center font-lato">
-            <hr className="bg-primary40 h-1 w-[40%]" />
+            <hr className="bg-primary40 h-1 w-[45%]" />
             <p className="w-full text-center text-primary40">Sign up and join partnership</p>
-            <hr className="bg-primary40 h-1 w-[40%]" />
+            <hr className="bg-primary40 h-1 w-[45%]" />
         </div>
     )
 }
--- src/components/auth/resetpassword.tsx
+++ src/components/auth/resetpassword.tsx
@@ -4,26 +4,28 @@ import * as z from "zod";
 
 import { Button } from "../ui/button";
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
 } from "../ui/form"
-import {useState } from "react";
+import { useState } from "react";
 import { ReloadIcon } from "@radix-ui/react-icons";
 import { IconInput } from "../global/iconinput";
 import { Text } from "../global/text";
 import { ResetPasswordformSchema } from "../../lib/schema";
 import { useNavigate } from "react-router-dom";
 import { Bounce, toast, ToastContainer } from "react-toastify";
+import Authprompt from "./authprompt";
+import Partnership from "./partnership";
 
 export function ResetPasswordForm() {
-    const[isLoading, setIsLoading]= useState<boolean>(false);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
     const form = useForm<z.infer<typeof ResetPasswordformSchema>>({
         resolver: zodResolver(ResetPasswordformSchema),
         defaultValues: {
-            email:""
+            email: ""
         },
     })
 
@@ -33,9 +35,9 @@ export function ResetPasswordForm() {
         // console.log(values)
         const {
             email
-        }=values;
+        } = values;
 
-        let userEmail = {email};
+        let userEmail = { email };
         setIsLoading(true)
 
         //fetch API 
@@ -52,21 +54,21 @@ export function ResetPasswordForm() {
             //log response error
             if (!response.ok) {
                 throw new Error('Unprocessablee content!');
-            } 
+            }
             else if (response.ok) {
                 response = await response.json()
                 console.log('Response: ', response);
                 localStorage.setItem("user-info", JSON.stringify(response))
 
                 //navigate to signin pg after successful reset
-                setTimeout(()=> navigate('/auth/signin'), 2000); //delayed to allow toast display
+                setTimeout(() => navigate('/auth/signin'), 2000); //delayed to allow toast display
 
                 toast.success("Password reset link sent to your email!", {
                     position: "bottom-right",
                     draggable: true
                 })
             }
-        } catch(error) {
+        } catch (error) {
             console.error("error:", error);
             toast.error("No user with that email address.", {
                 position: "bottom-right",
@@ -75,66 +77,61 @@ export function ResetPasswordForm() {
         } finally {
             setIsLoading(false);
         }
-        
+
     }
 
-  return (
-    <div className="flex flex-col my-5">
-        <Text
-            style="text-xl font-semibold mb-4 text-center"
-            value="FORGET PASSWORD"
-        />
-        <Text
-            style="text-md font-light mb-8 text-center"
-            value="ENTER YOUR EMAIL TO RESET PASSWORD"
-        />
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">  
-            <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                    <FormItem>
-                    <FormControl>
-                        <IconInput
-                            style="p-2"
-                            icon={
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
-                                </svg>
-                            }
-                            type="email"
-                            category="formInput"
-                            placeHolder="example@gmail.com" 
-                            field = {field}
-                        />
-                    </FormControl>
-                    <FormMessage />
-                    </FormItem>
-                )}
-                />
-                
-                <div 
-                    className="flex justify-end items-end bg-[#24243E] rounded-md mt-4"
-                    style={{marginTop:"1rem"}}
-                >
-                    {isLoading?
-                        <Button 
-                            disabled 
-                            className="w-full text-white bg-[#24243E] p-[0.5rem]">
-                            <ReloadIcon className="mr-2 h-4 w-4 animate-spin text-white" />
-                            Please wait...
-                        </Button>:
-                        <Button type="submit" className="w-full text-white bg-[#24243E] p-[0.5rem]" style={{display: "flex", justifyContent: "space-between", padding: "0px 30px"}} >Proceed to my Account <i className="fa-solid fa-arrow-right"></i></Button>
-                    }
-                    <ToastContainer transition={Bounce} draggable/>                
-                </div>
-            </form>
-        </Form>
+    return (
+        <div className="flex flex-col my-5">
 
-        <div className="bottom absolute bottom-0 mb-3" style={{fontSize: "12px", marginLeft:"20%"}}>
-            <a href=""><h6>Having issues with your Password?</h6></a>
+            <Text
+                style="text-xl font-lato font-bold leading-[140%] mb-4 text-primary60 text-center"
+                value="FORGET PASSWORD"
+            />
+
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <IconInput
+                                        type="email"
+                                        category="formInput"
+                                        placeHolder="Email Address"
+                                        field={field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <div
+                        className="flex justify-end items-end bg-[#24243E] rounded-md mt-4">
+                        {isLoading ? (
+                            <Button
+                                disabled
+                                className="w-full h-12 text-white bg-[#24243E] p-[0.5rem]"
+                            >
+                                <ReloadIcon className="mr-2 h-4 w-4 animate-spin text-white" />
+                                Please wait...
+                            </Button>
+                        ) : (
+                            <Button
+                                type="submit"
+                                className="w-full text-base flex justify-center text-white font-bold h-12 bg-[#24243E] p-[0.5rem]"
+                            >
+                                Forget Password
+                            </Button>
+                        )}
+                        <ToastContainer transition={Bounce} draggable />
+                    </div>
+                    <Authprompt action="reset" />
+                </form>
+                <Partnership />
+            </Form>
         </div>
-    </div>
-  )
+    )
 }
