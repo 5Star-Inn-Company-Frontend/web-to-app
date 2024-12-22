743b2410d83076d2d6ab77e7590568bbeb589f42
Godwin Opara <godwinopara62@gmail.com>
2024-12-22 11:06:02 +0100

    feat:styled active link to have gray background

--- src/components/global/routeBtn.tsx
+++ src/components/global/routeBtn.tsx
@@ -14,11 +14,12 @@ export const RouteBtn = ({
   pathname: string;
 }) => {
   const navigate = useNavigate();
+
   return (
     <li className="relative hover:bg-deepgray rounded-md px-1" onClick={() => navigate(`/app${route}`)}>
       <Link
         className={`${
-          pathname === `/dashboard${route}` ? 'bg-[#ecfdf5]' : ''
+          pathname === `/app${route}` ? 'bg-deepgray px-1' : ''
         } group flex h-12 cursor-pointer items-center truncate rounded-[5px] px-0.1 py-4 text-[0.975rem] text-gray-700 outline-none transition duration-300 ease-linear hover:text-[red]-600 hover:outline-none focus:bg-[red]-400/10 focus:text-[red]-600 focus:outline-none active:bg-[red]-400/10 active:text-[red]-600 active:outline-none data-[te-sidenav-state-active]:text-[red]-600 data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10`}
         to="#"
         data-te-sidenav-link-ref
