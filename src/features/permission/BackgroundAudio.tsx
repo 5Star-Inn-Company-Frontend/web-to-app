5dd69b275a2c74c505cc308bd2b7a4f45fea3808
godwin opara <godwin.opara@ng.ab-inbev.com>
2025-02-27 20:47:41 +0100

    feat(Background Audio): Extracted Background audio section on App permission page and integrated with backend api

--- src/features/permission/BackgroundAudio.tsx
+++ src/features/permission/BackgroundAudio.tsx
@@ -1,7 +1,22 @@
 import { CollapsibleComponent } from "@/components/global/collapsibleComponent";
 import { OsConfigCard } from "@/components/global/os_config_card";
+import { updatePermission } from "@/redux/app/appSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hook";
+import { RootState } from "@/redux/store";
+import { useState } from "react";
 
 export default function BackgroundAudio() {
+    const dispatch = useAppDispatch();
+    const backgroundAudio = useAppSelector((state: RootState) => state.app.permission.background_audio);
+
+    const initialValue = backgroundAudio ? "enable" : "disable";
+    const [enableBackgroundAudio, setEnableBackgroundAudio] = useState(initialValue);
+
+    const handleChangeBackgroundAudio = (value: string) => {
+        setEnableBackgroundAudio(value);
+        const isBackgroundAudioEnabled = value === "enable";
+        dispatch(updatePermission({ background_audio: isBackgroundAudioEnabled }));
+    };
     return (
         <div className="px-6 py-4 bg-white border-b border-primary20">
             <CollapsibleComponent
@@ -11,6 +26,8 @@ export default function BackgroundAudio() {
                 <div className="w-1/2 px-8">
                     <OsConfigCard
                         os="Android"
+                        value={enableBackgroundAudio}
+                        onValueChange={handleChangeBackgroundAudio}
                         radios={[
                             { title: "Disable", label: "disable" },
                             { title: "Enable", label: "enable" },
