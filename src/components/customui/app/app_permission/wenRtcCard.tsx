d60f2becaa63d6342f46e4441de26734f217e658
godwin opara <godwin.opara@ng.ab-inbev.com>
2025-02-27 20:28:55 +0100

    feat(Download Directory location): extracted the download directory section on app permission page to a new component and integrated with backend api

--- src/components/customui/app/app_permission/wenRtcCard.tsx
+++ src/components/customui/app/app_permission/wenRtcCard.tsx
@@ -1,67 +1,63 @@
-import { Label } from "@/components/ui/label"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-import { Text } from "@/components/global/text"
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Text } from "@/components/global/text";
 
-export const WebRtcConfigCard =({
-    radios
-}:{
-    radios:{
-        title:string,
-        label:string
-    }[]
-})=>{
-    return(
+interface IWebRtcConfigCard {
+    micValue: string;
+    onMicValueChange: (value: string) => void;
+    camValue: string;
+    onCamValueChange: (value: string) => void;
+    radios: {
+        title: string;
+        label: string;
+    }[];
+}
+
+export const WebRtcConfigCard = ({
+    radios,
+    micValue,
+    camValue,
+    onMicValueChange,
+    onCamValueChange,
+}: IWebRtcConfigCard) => {
+    return (
         <div className="p-3">
-            <Text
-                style="text-xs font-light uppercase mb-2"
-                value="Microphone"
-            />
-            <RadioGroup 
+            <Text style="text-xs font-light uppercase mb-2" value="Microphone" />
+            <RadioGroup
                 className="flex justify-between border border-primary20 rounded-lg p-[0.5rem] flex-grow mb-2"
-                defaultValue="off">
-                    {
-                        radios.map((rad,index)=>{
-                            const{
-                                title,
-                                label
-                            }=rad;
-                            return(
-                                <div 
-                                    className="flex items-center space-x-2"
-                                    key={index}
-                                >
-                                    <RadioGroupItem value={label} id="r1" />
-                                    <Label className="text-xs" htmlFor={label}>{title}</Label>
-                                </div>
-                            )
-                        })
-                    }
+                value={micValue}
+                onValueChange={onMicValueChange}
+            >
+                {radios.map((rad, index) => {
+                    const { title, label } = rad;
+                    return (
+                        <div className="flex items-center space-x-2" key={index}>
+                            <RadioGroupItem value={label} id="r1" />
+                            <Label className="text-xs" htmlFor={label}>
+                                {title}
+                            </Label>
+                        </div>
+                    );
+                })}
             </RadioGroup>
-            <Text
-                style="text-xs font-light uppercase mb-2"
-                value="Camera"
-            />
-            <RadioGroup 
+            <Text style="text-xs font-light uppercase mb-2" value="Camera" />
+            <RadioGroup
                 className="flex justify-between border border-primary20 rounded-lg p-[0.5rem] flex-grow "
-                defaultValue="off">
-                    {
-                        radios.map((rad,index)=>{
-                            const{
-                                title,
-                                label
-                            }=rad;
-                            return(
-                                <div 
-                                    className="flex items-center space-x-2"
-                                    key={index}
-                                >
-                                    <RadioGroupItem value={label} id="r1" />
-                                    <Label className="text-xs" htmlFor={label}>{title}</Label>
-                                </div>
-                            )
-                        })
-                    }
+                value={camValue}
+                onValueChange={onCamValueChange}
+            >
+                {radios.map((rad, index) => {
+                    const { title, label } = rad;
+                    return (
+                        <div className="flex items-center space-x-2" key={index}>
+                            <RadioGroupItem value={label} id="r1" />
+                            <Label className="text-xs" htmlFor={label}>
+                                {title}
+                            </Label>
+                        </div>
+                    );
+                })}
             </RadioGroup>
         </div>
-    )
-}
+    );
+};
--- src/features/permission/WebRTC.tsx
+++ src/features/permission/WebRTC.tsx
@@ -1,8 +1,28 @@
 import { WebRtcConfigCard } from "@/components/customui/app/app_permission/wenRtcCard";
 import { CollapsibleComponent } from "@/components/global/collapsibleComponent";
 import { OsConfigCard } from "@/components/global/os_config_card";
+// import { useAppDispatch, useAppSelector } from "@/redux/hook";
+// import { RootState } from "@/redux/store";
+import { useState } from "react";
 
 export default function WebRTC() {
+    // const dispatch = useAppDispatch();
+    // const media = useAppSelector((state: RootState) => state.app.permission.media);
+
+    // const initialValue = {microphone: med}
+    const [enableMicrophone, setEnableMicrophone] = useState("disable");
+    const [enableCamera, setEnableCamera] = useState("disable");
+
+    // const [enableMedia, setEnableMedia] = useState({ microphone: "disable", camera: "disable" });
+
+    const handleChangeEnableCamera = (value: string) => {
+        setEnableCamera(value);
+    };
+
+    const handleChangeEnableMicrophone = (value: string) => {
+        setEnableMicrophone(value);
+    };
+
     return (
         <div className="px-6 py-4 bg-white border-b border-primary20">
             <CollapsibleComponent
@@ -12,6 +32,10 @@ export default function WebRTC() {
                 <div className="w-1/2 px-8">
                     <OsConfigCard os="Android">
                         <WebRtcConfigCard
+                            onCamValueChange={handleChangeEnableCamera}
+                            onMicValueChange={handleChangeEnableMicrophone}
+                            micValue={enableMicrophone}
+                            camValue={enableCamera}
                             radios={[
                                 { title: "Disable", label: "disable" },
                                 { title: "Enable", label: "enable" },
